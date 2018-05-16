83ccebefb24fa94a4e08fc710e4e0b4b331eb7da
于阔源 <yukuoyuan@hotmail.com>
2018-05-16 20:53:31 +0800

    提交了底部弹窗的修改

--- src/components/shopcart/Shopcart.vue
+++ src/components/shopcart/Shopcart.vue
@@ -18,19 +18,22 @@
       </div>
     </div>
     <div class="ball-container">
-      <transition-group name="drop" tag="div" @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
-        <div class="ball" v-for="(ball,index) of balls" v-show="ball.show" :key="index">
-        </div>
-      </transition-group>
-    </div>
-    <div class="short-cart-list" v-show="listShow">
-      <div class="list-header">
-        <h1 class="title">购物车</h1>
-        <span class="empty">清空</span>
+      <div v-for="(ball,index) in balls" :key="index">
+        <transition name="drop" @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+          <div class="ball" v-show="ball.show">
+            <div class="inner inner-hook"></div>
+          </div>
+        </transition>
       </div>
-      <div class="list-content">
-        <ul>
-          <li v-for="(food,index) of selectFoods" :key="index" class="food">
+    </div>
+    <transition name="fold">
+      <div class="short-cart-list" v-show="listShow">
+        <div class="list-header">
+          <h1 class="title">购物车</h1>
+          <span class="empty">清空</span>
+        </div>
+        <div class="list-content">
+          <div v-for="(food,index) of selectFoods" :key="index" class="food">
             <span class="name">{{food.name}}</span>
             <div class="price">
               <span>${{food.price*food.count}}</span>
@@ -38,10 +41,10 @@
             <div class="cartcontrol-wrapper">
               <cart-control :food="food"></cart-control>
             </div>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -150,38 +153,41 @@
       // 进入中
       // --------
       beforeEnter: function (el) {
-        this.balls.forEach((ball) => {
+        let count = this.balls.length;
+        while (count--) {
+          let ball = this.balls[count];
           if (ball.show) {
-            let react = ball.el.getBoundingClientRect()
-            /**
-             * 获取小球xy的偏移
-             * @type {number}
-             */
-            let x = react.x - 32
-            let y = -(window.innerHeight - react.top - 22)
-            el.style.webkitTransform = `translate3d(${x}px,${y}px,0)`
-            el.style.transform = `translate3d(${x}px,${y}px,0)`
+            let rect = ball.el.getBoundingClientRect();
+            let x = rect.left - 32;
+            let y = -(window.innerHeight - rect.top - 22);
+            el.style.display = '';
+            el.style.webkitTransform = `translate3d(0,${y}px,0)`;
+            el.style.transform = `translate3d(0,${y}px,0)`;
+            let inner = el.getElementsByClassName('inner-hook')[0];
+            inner.style.webkitTransform = `translate3d(${x}px,0,0)`;
+            inner.style.transform = `translate3d(${x}px,0,0)`;
           }
-        })
+        }
       },
       // 此回调函数是可选项的设置
       // 与 CSS 结合时使用
       enter: function (el, done) {
         /* eslint-disable no-unused-vars */
-        let ref = el.offsetHeight
+        let rf = el.offsetHeight;
         this.$nextTick(() => {
-          el.style.webkitTransform = 'translate3d(0,0,0)'
-          el.style.transform = 'translate3d(0,0,0)'
-        })
-        done()
+          el.style.webkitTransform = 'translate3d(0,0,0)';
+          el.style.transform = 'translate3d(0,0,0)';
+          let inner = el.getElementsByClassName('inner-hook')[0];
+          inner.style.webkitTransform = 'translate3d(0,0,0)';
+          inner.style.transform = 'translate3d(0,0,0)';
+          el.addEventListener('transitionend', done);
+        });
       },
       afterEnter: function (el) {
-        /**
-         * 删除并返回第一个元素
-         */
-        let ball = this.dropBalls.shift()
+        let ball = this.dropBalls.shift();
         if (ball) {
-          ball.show = false
+          ball.show = false;
+          el.style.display = 'none';
         }
       }
     },
@@ -208,16 +214,16 @@
         flex 1
         .logo-wrapper
           display inline-block
-          position relative
-          top -10px
-          margin 0 12px
-          padding 6px
-          width 56px
-          height 56px
-          box-sizing border-box
           vertical-align top
-          border-radius 50%
-          background #141d27
+          position: relative
+          top: -10px
+          margin: 0 12px
+          padding: 6px
+          width: 56px
+          height: 56px
+          box-sizing: border-box
+          border-radius: 50%
+          background: #141d27
           .logo
             width 100%
             height 100%
@@ -233,29 +239,29 @@
               &.highlight
                 color white
         .num
-          position absolute
-          top 0
-          right 0
-          width 24px
-          height 16px
-          line-height 16px
-          text-align center
-          border-radius 16px
-          font-size 9px
-          font-weight bold
-          color #fff
-          background rgb(240, 20, 20)
-          box-shadow 0 4px 8px 0 rgba(0, 0, 0, 0.4)
+          position: absolute
+          top: 0
+          right: 0
+          width: 24px
+          height: 16px
+          line-height: 16px
+          text-align: center
+          border-radius: 16px
+          font-size: 9px
+          font-weight: 700
+          color: #fff
+          background: rgb(240, 20, 20)
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
         .price
-          display inline-block
-          vertical-align top
-          margin-top 12px
+          display: inline-block
+          vertical-align: top
+          margin-top: 12px
           line-height: 24px
-          padding-right 12px
-          box-sizing border-box
-          border-right 1px solid rgba(255, 255, 255, 0.1)
-          font-size 16px
-          font-weight bold
+          padding-right: 12px
+          box-sizing: border-box
+          border-right: 1px solid rgba(255, 255, 255, 0.1)
+          font-size: 16px
+          font-weight: 700
           &.highlight
             color white
         .desc
@@ -265,9 +271,8 @@
           margin 12px 0 0 12px
           font-size 10px
       .content-right
-        flex 0 0 105px
-        width 105px
-        background #2b333b
+        flex: 0 0 105px
+        width: 105px
         .pay
           height 48px
           line-height 48px
@@ -282,20 +287,27 @@
     .ball-container
       .ball
         position: fixed
-        left 32px
-        bottom 22px
+        left: 32px
+        bottom: 22px
         z-index: 200
-        width 16px
-        height 16px
-        border-radius 50%
-        background rgb(0, 160, 220)
-        transition all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
+        transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
+        .inner
+          width: 16px
+          height: 16px
+          border-radius: 50%
+          background: rgb(0, 160, 220)
+          transition: all 0.4s linear
     .short-cart-list
       position absolute
       top: 0
       left 0
-      z-index 1
+      z-index -1
       width 100%
+      transform: translate3d(0, -100%, 0)
+      &.fold-enter-active, &.fold-leave-active
+        transition: all 0.5s
+      &.fold-enter, &.fold-leave-active
+        transform: translate3d(0, 0, 0)
       .list-header
         height 40px
         line-height 40px
@@ -325,13 +337,13 @@
             font-size 14px
             color rgb(7, 17, 27)
           .price
-            position: absolute;
-            right 10px
-            bottom 12px
-            line-height 24px
-            font-size 14px
-            font-weight bold
-            color rgb(240, 20, 20)
+            position: absolute
+            right: 90px
+            bottom: 12px
+            line-height: 24px
+            font-size: 14px
+            font-weight: 700
+            color: rgb(240, 20, 20)
           .cartcontrol-wrapper
             position: absolute;
             bottom 6px
